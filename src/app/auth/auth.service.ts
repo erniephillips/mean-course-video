7ffb27d1349bc0ca22c9be471b1dbd38f7f75121
Ernie Phillips <eraphillips@my365.bellevue.edu>
2020-03-08 22:13:12 -0400

    added feature to auto logout user after 1 hour

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { Router } from "@angular/router";
 export class AuthService {
   private isAuthenticated = false;
   private token: string;
+  private tokenTimer: any;
   private authStatusListener = new Subject<boolean>(); //subject is used to push auth info to the components that are interested
 
   constructor(private http: HttpClient, private router: Router) {}
@@ -38,11 +39,18 @@ export class AuthService {
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
-      .post<{ token: string }>("http://localhost:3000/api/user/login", authData)
+      .post<{ token: string; expiresIn: number }>(
+        "http://localhost:3000/api/user/login",
+        authData
+      )
       .subscribe(response => {
         const token = response.token;
         this.token = token;
         if (token) {
+          const expiresInDuration = response.expiresIn;
+          this.tokenTimer = setTimeout(() => { //force logout after 1 hour
+            this.logout();
+          }, expiresInDuration * 1000);
           this.isAuthenticated = true;
           this.authStatusListener.next(true);
           this.router.navigate(["/"]);
@@ -54,6 +62,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    clearTimeout(this.tokenTimer);
     this.router.navigate(["/"]);
   }
 }
